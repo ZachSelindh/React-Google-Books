95ad4f3c33b6807169cbd4305b8a806d7dc73678
Zach Selindh <zachselindh@gmail.com>
2019-05-30 21:05:36 -0500

    Modified server for proper build location.

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ const PORT = process.env.PORT || 3001;
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("/ping", function(req, res) {
   return res.send("pong");
 });
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 // Start the API server
