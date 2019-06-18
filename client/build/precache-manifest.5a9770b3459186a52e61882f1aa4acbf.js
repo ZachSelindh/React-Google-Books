433c6a8b554b0d7d8ce67340115166c9388bfe30
Zach Selindh <zachselindh@gmail.com>
2019-06-18 16:34:27 -0500

    Added npm build.

---
+++ client/build/precache-manifest.5a9770b3459186a52e61882f1aa4acbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de76b37a92adc71d170798c7381b0c8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8dc9cff1e675c42d6f5",
+    "url": "/static/css/main.f11ccdc3.chunk.css"
+  },
+  {
+    "revision": "dcaf195bb2b7ebdda97c",
+    "url": "/static/js/2.82f98849.chunk.js"
+  },
+  {
+    "revision": "c8dc9cff1e675c42d6f5",
+    "url": "/static/js/main.818f1e91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
