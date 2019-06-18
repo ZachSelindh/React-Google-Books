1e151436c606fc3205ed56ab05070a3ffa0ae9f4
Zach Selindh <zachselindh@gmail.com>
2019-06-18 16:17:19 -0500

    Redeploying.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.use("/api", apiRoutes);
 // Send React start point if no API routes are called.
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  // Non-build path
+  /* res.sendFile(path.join(__dirname, "../client/index.html")); */
 });
 
 module.exports = router;
